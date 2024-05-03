66ed4d7421e8696cb0217497706c562e9c08baa2
Araozu <fernando@araozu.dev>
2024-05-02 19:25:46 -0500

    Remove initial files, clean workspace

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import type { Component } from "solid-js";
-
-import logo from "./logo.svg";
-import styles from "./App.module.css";
-
-const App: Component = () => (
-    <div class={styles.App}>
-        <header class={styles.header}>
-            <img src={logo} class={styles.logo} alt="logo" />
-            <p class="text-red-400 font-black border border-green-500 rounded-lg py-2 px-4">
-                Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-                class={styles.link}
-                href="https://github.com/solidjs/solid"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn Solid
-            </a>
-        </header>
-    </div>
-);
-
-export default App;
+export default function() {
+    return (
+        <div class="text-slate-600 font-black text-2xl">
+            Hola mundo :D
+        </div>
+    );
+}
